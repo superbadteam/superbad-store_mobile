71ff921f5fa79190b51a2cc42cb43de8d0fd7653
phduchuy <phanhoangduchuyy@gmail.com>
2024-05-08 10:57:29 +0700

    [SBS-12] update font

---
+++ app/screens/ProductDetailScreen/SlideShow.tsx
@@ -0,0 +1,42 @@
+import React from "react";
+import { StyleSheet, View, Dimensions, Image } from "react-native";
+import Swiper from "react-native-swiper";
+
+const { width } = Dimensions.get("window");
+
+interface SlideShowProps {
+  images: string[];
+}
+
+const SlideShow: React.FC<SlideShowProps> = ({ images }) => {
+  return (
+    <View style={styles.container}>
+      <Swiper style={styles.wrapper} showsButtons={false} autoplay>
+        {images.map((imageUri, index) => (
+          <View key={index} style={styles.slide}>
+            <Image source={{ uri: imageUri }} style={styles.image} />
+          </View>
+        ))}
+      </Swiper>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    height: 300,
+  },
+  image: {
+    borderRadius: 5,
+    height: 300,
+    width,
+  },
+  slide: {
+    alignItems: "center",
+    flex: 1,
+    justifyContent: "center",
+  },
+  wrapper: {},
+});
+
+export default SlideShow;
