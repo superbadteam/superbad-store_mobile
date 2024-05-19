872cafd39c0294d411ef6f27c24b0294c89a74e7
phduchuy <phanhoangduchuyy@gmail.com>
2024-05-19 10:14:54 +0700

    [SBS-43] refactor: remove style of touchable opacity

--- app/components/BackButton.tsx
+++ app/components/BackButton.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { Ionicons } from "@expo/vector-icons";
 import { useNavigation } from "@react-navigation/native";
-import { TextStyle, TouchableOpacity } from "react-native";
+import { TouchableOpacity } from "react-native";
 
 interface BackButtonProps {
   tintColor: string;
@@ -11,14 +11,10 @@ const BackButton: React.FC<BackButtonProps> = ({ tintColor }) => {
   const navigation = useNavigation();
 
   return (
-    <TouchableOpacity style={$iconContainer} onPress={() => navigation.goBack()}>
+    <TouchableOpacity onPress={() => navigation.goBack()}>
       <Ionicons name="arrow-back" size={24} color={tintColor} />
     </TouchableOpacity>
   );
-};
-
-const $iconContainer: TextStyle = {
-  padding: 0,
 };
 
 export default BackButton;
