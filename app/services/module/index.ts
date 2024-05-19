ffd5bbbb84f84d436235cb8ddde0197b67c0b86e
phduchuy <phanhoangduchuyy@gmail.com>
2024-05-19 14:14:08 +0700

    [SBS-23] setup: init code integrate product detail

--- app/services/module/index.ts
+++ app/services/module/index.ts
@@ -1,7 +1,9 @@
 import * as Inventory from "./inventory";
+import * as Shopping from "./shopping";
 
 const ApiService = {
   inventory: Inventory,
+  shopping: Shopping,
 };
 
 export default ApiService;
