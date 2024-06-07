3742afc1be06a24c28ceca133b5bf6fedb1f2004
huynamboz <teahuynam2003@gmail.com>
2024-06-07 10:12:16 +0700

    fix: useSWR

--- app/services/hooks/useInventory.tsx
+++ app/services/hooks/useInventory.tsx
@@ -1,28 +1,33 @@
-import useSWRMutation from "swr/mutation";
+import useSWR from "swr";
 import { notification } from "antd";
 import ApiService from "../modules";
 import { ApiErrorResponse } from "../api/api.types";
 import { Category } from "app/types";
 
 export const useGetCategories = () => {
-  const { trigger: getCategories, isMutating } = useSWRMutation(
+  const {
+    data: categories,
+    error,
+    mutate: getCategories,
+    isValidating: isMutating,
+  } = useSWR(
     "api/inventory/categories",
     async () => {
       return await ApiService.inventory.getCategories();
     },
     {
-      onError: (error: ApiErrorResponse) => {
-        notification.error({
-          message: error.title,
-        });
-      },
-      onSuccess: (data: Category[]) => {
-        return data;
-      },
+      revalidateOnFocus: false,
     },
   );
 
+  if (error) {
+    notification.error({
+      message: (error as ApiErrorResponse).title,
+    });
+  }
+
   return {
+    categories,
     getCategories,
     isMutating,
   };
